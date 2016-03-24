2117d46b7a60a5a2700e999f5b9f427a0012da87
Inmaculada Martínez Lobo <uo238739@uniovi.es>
2016-03-24 16:39:27 +0100

    Creados ficheros de propiedades y añadidos mensajes de log

--- sdi2-23/src/uo/sdi/presentation/BeanLocaleSettings.java
+++ sdi2-23/src/uo/sdi/presentation/BeanLocaleSettings.java
@@ -1,6 +1,7 @@
 package uo.sdi.presentation;
 
 import java.util.Locale;
+import alb.util.log.Log;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
@@ -21,23 +22,27 @@ public class BeanLocaleSettings {
 	   locale = FacesContext.getCurrentInstance()
 	   	.getExternalContext().getRequestLocale();
 	   
-	   /*si el idioma del navegador no esta soportado 
+	   /* si el idioma del navegador no esta soportado 
 	    * se cambia locale a ingles
 	    */
-	   if(!locale.equals(ENGLISH) || !locale.equals(SPANISH)){
+	   if(!locale.equals(ENGLISH) && !locale.equals(SPANISH)){
 	       locale = ENGLISH; 
 	   }
+	   Log.info("Se ha establecido el idioma como [%s]", 
+		   locale.getDisplayName());
 	}
-	return (locale);
+	return locale;
     }
 
     public void setSpanish(ActionEvent event) {
 	locale = SPANISH;
 	FacesContext.getCurrentInstance().getViewRoot().setLocale(locale);
+	Log.info("Se ha cambiado el idioma a [%s]", locale.getDisplayName());
     }
 
     public void setEnglish(ActionEvent event) {
 	locale = ENGLISH;
 	FacesContext.getCurrentInstance().getViewRoot().setLocale(locale);
+	Log.info("Se ha cambiado el idioma a [%s]", locale.getDisplayName());
     }
 }
