018e57a5bf13a513b8edb88196b97ea93e5baf85
Inmaculada Martínez Lobo <uo238739@uniovi.es>
2016-03-29 18:40:39 +0200

    LoginListener implementado

--- sdi2-23/src/uo/sdi/presentation/BeanUser.java
+++ sdi2-23/src/uo/sdi/presentation/BeanUser.java
@@ -16,83 +16,80 @@ import uo.sdi.transport.UserDTO;
 public class BeanUser {
 
     private UserDTO user;
-    
-    public boolean isPublico(){
-	return user==null;
+
+    public boolean isPublico() {
+	return user == null;
     }
-    
+
     public Long getId() {
 	return user.getId();
     }
 
     public String getLogin() {
-        return user.getLogin();
+	return user.getLogin();
     }
 
     public String getPassword() {
-        return user.getPassword();
+	return user.getPassword();
     }
 
     public String getName() {
-        return user.getName();
+	return user.getName();
     }
 
     public String getSurname() {
-        return user.getSurname();
+	return user.getSurname();
     }
 
     public String getEmail() {
-        return user.getEmail();
+	return user.getEmail();
     }
 
     public Set<TripDTO> getPromotedTrips() {
-        return user.getPromotedTrips();
+	return user.getPromotedTrips();
     }
 
-    public UserDTO getCurrentUser(){
+    public UserDTO getCurrentUser() {
 	return user;
     }
-    
-    public void setCurrentUser(UserDTO user){
+
+    public void setCurrentUser(UserDTO user) {
 	this.user = user;
     }
-    
-    public String cerrarSesion(){
-	 FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
-	 return "exito";
+
+    public String cerrarSesion() {
+	user = null;
+	FacesContext.getCurrentInstance().getExternalContext()
+		.invalidateSession();
+	return "exito";
     }
-    
-    public String cancelarPlaza(TripDTO viaje){
-	try{
-            Factories.services.createUserService()
-                    .cancelSeat(user.getId(), viaje);
-            Log.info("Se está cancelando la plaza para el viaje " +
-                    "[%d] al usuario [%d]", viaje.getId(), user.getId());
-            return "exito";
-	} catch (Exception e){
+
+    public String cancelarPlaza(TripDTO viaje) {
+	try {
+	    Factories.services.createUserService().cancelSeat(user.getId(),
+		    viaje);
+	    Log.info("Se está cancelando la plaza para el viaje "
+		    + "[%d] al usuario [%d]", viaje.getId(), user.getId());
+	    return "exito";
+	} catch (Exception e) {
 	    Log.debug("Ha ocurrido una [%s] cancelando la plaza "
-	    	+ "del usuario [%d] en el viaje [%d]: [%s]", 
-	    	e.getClass().toString(),
-	    	user.getId(),
-	    	viaje.getId(),
-	    	e.getMessage());
+		    + "del usuario [%d] en el viaje [%d]: [%s]", e.getClass()
+		    .toString(), user.getId(), viaje.getId(), e.getMessage());
 	    return "error";
 	}
     }
-    
-    public String solicitarPlaza(TripDTO viaje){
-	try{
-	    Log.info("Se está solicitando plaza para el viaje " +
-                    "[%d] al usuario [%d]", viaje.getId(), user);
-            Factories.services.createUserService().requestSeat(user, viaje);
-            return "exito";
-	} catch (Exception e){
+
+    public String solicitarPlaza(TripDTO viaje) {
+	try {
+	    Log.info("Se está solicitando plaza para el viaje "
+		    + "[%d] al usuario [%d]", viaje.getId(), user);
+	    Factories.services.createUserService().requestSeat(user, viaje);
+	    return "exito";
+	} catch (Exception e) {
 	    Log.debug("Ha ocurrido una [%s] solicitando plaza "
-	    	+ "para el usuario [%d] en el viaje [%d]: [%s]", 
-	    	e.getClass().toString(),
-	    	user.getId(),
-	    	viaje.getId(),
-	    	e.getMessage());
+		    + "para el usuario [%d] en el viaje [%d]: [%s]", e
+		    .getClass().toString(), user.getId(), viaje.getId(), e
+		    .getMessage());
 	    return "error";
 	}
     }
