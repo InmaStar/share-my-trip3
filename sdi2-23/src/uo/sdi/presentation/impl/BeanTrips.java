b46b492d6bc1a333ff65e0fbfff451999f990e37
Inmaculada Martínez Lobo <uo238739@uniovi.es>
2016-04-02 22:02:57 +0200

    Añadidos checkboxes para cancelacion multiple

--- sdi2-23/src/uo/sdi/presentation/impl/BeanTrips.java
+++ sdi2-23/src/uo/sdi/presentation/impl/BeanTrips.java
@@ -2,8 +2,12 @@ package uo.sdi.presentation.impl;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
+import java.util.Set;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
@@ -20,70 +24,94 @@ import uo.sdi.util.bundle.BundleLoader;
 
 @ManagedBean(name = "trips")
 @SessionScoped
-public class BeanTrips implements Serializable{
+public class BeanTrips implements Serializable {
     /**
      * 
      */
     private static final long serialVersionUID = 1L;
     List<TripDTO> viajes = new ArrayList<TripDTO>();
+    Map<TripDTO, Boolean> viajesSeleccionados;
 
     public BeanTrips() {
 	listadoDisponibles();
+	viajesSeleccionados = new HashMap<TripDTO, Boolean>();
     }
 
     public List<TripDTO> getListaViajes() {
 	return viajes;
     }
-    
+
+    public Map<TripDTO, Boolean> getSeleccionados() {
+	return viajesSeleccionados;
+    }
+
+    public boolean ningunSeleccionado() {
+	return viajesSeleccionados.isEmpty();
+    }
+
     public String cancelarViaje(TripDTO viaje, Long userId) {
-	try{
+	try {
 	    TripService tripServ = Factories.services.createTripService();
 	    tripServ.cancel(viaje);
 	    Log.info("El viaje [%d] ha sido cancelado", viaje.getId());
 	    listadoPromotor(userId);
 	    return "exito";
-	} catch(Exception e){
-	    Log.debug("Ha ocurrido una [%s] cancelando el viaje [%d]: [%s]", 
-		    e.getClass().toString(),
-		    viaje.getId(),
-		    e.getMessage());
+	} catch (Exception e) {
+	    Log.debug("Ha ocurrido una [%s] cancelando el viaje [%d]: [%s]", e
+		    .getClass().toString(), viaje.getId(), e.getMessage());
 	    return "error";
 	}
+    }
+
+    public void cancelarViajes(Long userId) {
+	Set<TripDTO> viajes = viajesSeleccionados.keySet();
+	TripDTO viaje;
+	Iterator<TripDTO> it = viajes.iterator();
+	while (it.hasNext()) {
+	    viaje = it.next();
+	    try {
+		Factories.services.createTripService().cancel(viaje);
+		Log.info("El viaje [%d] ha sido cancelado", viaje.getId());
+	    } catch (Exception e) {
+		Log.debug(
+			"Ha ocurrido una [%s] cancelando el viaje [%d]: [%s]",
+			e.getClass().toString(), viaje.getId(), e.getMessage());
+	    }
+	}
+	listadoPromotor(userId);
     }
 
     public String listadoDisponibles() {
 	try {
 	    List<TripDTO> listaViajes = cargarViajes();
 	    viajes = new ArrayList<TripDTO>();
-	    for(TripDTO viaje : listaViajes){
-		if(viaje.isVisible()){
+	    for (TripDTO viaje : listaViajes) {
+		if (viaje.isVisible()) {
 		    viajes.add(viaje);
 		}
 	    }
 	    return "exito";
 	} catch (Exception e) {
 	    Log.debug("Ha ocurrido una [%s] listando "
-	    	+ "los viajes disponibles: [%s]", 
-		    e.getClass().toString(),
+		    + "los viajes disponibles: [%s]", e.getClass().toString(),
 		    e.getMessage());
 	    return "error";
 	}
     }
-    
+
     public String listadoRelacionados(Long userId) {
 	try {
 	    List<TripDTO> listaViajes = cargarViajes();
 	    viajes = new ArrayList<TripDTO>();
-	    for(TripDTO viaje : listaViajes){
-		if(viaje.hasRelationship(userId)){
+	    for (TripDTO viaje : listaViajes) {
+		if (viaje.hasRelationship(userId)) {
 		    viajes.add(viaje);
 		}
 	    }
 	    return "exito";
 	} catch (Exception e) {
 	    Log.debug("Ha ocurrido una [%s] listando "
-	    	+ "los viajes relacionados: [%s]", 
-		    e.getClass().toString(),
+		    + "los viajes relacionados: [%s]", e.getClass().toString(),
 		    e.getMessage());
 	    return "error";
 	}
@@ -93,63 +121,65 @@ public class BeanTrips implements Serializable{
 	try {
 	    List<TripDTO> listaViajes = cargarViajes();
 	    viajes = new ArrayList<TripDTO>();
-	    for(TripDTO viaje : listaViajes){
-		if(viaje.isPromoter(userId)){
+	    for (TripDTO viaje : listaViajes) {
+		if (viaje.isPromoter(userId)) {
 		    viajes.add(viaje);
 		}
 	    }
+
+	    // inicializar la lista de seleccionados
+	    viajesSeleccionados.clear();
+
 	    return "exito";
 	} catch (Exception e) {
 	    Log.debug("Ha ocurrido una [%s] listando "
-	    	+ "los viajes relacionados: [%s]", 
-		    e.getClass().toString(),
+		    + "los viajes relacionados: [%s]", e.getClass().toString(),
 		    e.getMessage());
 	    return "error";
 	}
     }
-    
-    public String cancelarPlaza(TripDTO viaje, UserDTO usuario) {
-   	try {
-   	    Log.info("Se está cancelando la plaza para el viaje "
-   		    + "[%d] al usuario [%d]", viaje.getId(), usuario.getId());
-   	    Factories.services.createUserService()
-   		    .cancelApplication(usuario, viaje);
-   	    listadoDisponibles();
-   	    return "exito";
-   	} catch (Exception e) {
-   	    Log.debug("Ha ocurrido una [%s] cancelando la plaza "
-   		    + "del usuario [%d] en el viaje [%d]: [%s]", e.getClass()
-   		    .toString(), usuario.getId(), viaje.getId(), e.getMessage());
-   	    return "error";
-   	}
-       }
 
-       public String solicitarPlaza(TripDTO viaje, UserDTO usuario) {
-   	try {
-   	    Log.info("Se está solicitando plaza para el viaje "
-   		    + "[%d] al usuario [%d]", viaje.getId(), usuario.getId());
-   	   Factories.services.createUserService()
-   		    .requestSeat(usuario, viaje);
-   	    listadoDisponibles();
-   	    return "exito";
-   	} catch (Exception e) {
-   	    Log.debug("Ha ocurrido una [%s] solicitando plaza "
-   		    + "para el usuario [%d] en el viaje [%d]: [%s]", e
-   		    .getClass().toString(), usuario.getId(), viaje.getId(), e
-   		    .getMessage());
-   	    return "error";
-   	}
-       }
-    
-    public String localizarTripStatus(TripStatus status){
+    public String cancelarPlaza(TripDTO viaje, UserDTO usuario) {
+	try {
+	    Log.info("Se está cancelando la plaza para el viaje "
+		    + "[%d] al usuario [%d]", viaje.getId(), usuario.getId());
+	    Factories.services.createUserService().cancelApplication(usuario,
+		    viaje);
+	    listadoDisponibles();
+	    return "exito";
+	} catch (Exception e) {
+	    Log.debug("Ha ocurrido una [%s] cancelando la plaza "
+		    + "del usuario [%d] en el viaje [%d]: [%s]", e.getClass()
+		    .toString(), usuario.getId(), viaje.getId(), e.getMessage());
+	    return "error";
+	}
+    }
+
+    public String solicitarPlaza(TripDTO viaje, UserDTO usuario) {
+	try {
+	    Log.info("Se está solicitando plaza para el viaje "
+		    + "[%d] al usuario [%d]", viaje.getId(), usuario.getId());
+	    Factories.services.createUserService().requestSeat(usuario, viaje);
+	    listadoDisponibles();
+	    return "exito";
+	} catch (Exception e) {
+	    Log.debug("Ha ocurrido una [%s] solicitando plaza "
+		    + "para el usuario [%d] en el viaje [%d]: [%s]", e
+		    .getClass().toString(), usuario.getId(), viaje.getId(), e
+		    .getMessage());
+	    return "error";
+	}
+    }
+
+    public String localizarTripStatus(TripStatus status) {
 	ResourceBundle bundle = BundleLoader.load("msgs");
 	return bundle.getString(status.name());
     }
-    
-    public String localizarTripRelationship(UserTripRelationship relationship){
-   	ResourceBundle bundle = BundleLoader.load("msgs");
-   	return bundle.getString(relationship.name());
-       }
+
+    public String localizarTripRelationship(UserTripRelationship relationship) {
+	ResourceBundle bundle = BundleLoader.load("msgs");
+	return bundle.getString(relationship.name());
+    }
 
     private List<TripDTO> cargarViajes() throws BusinessException {
 	List<TripDTO> listaViajes = Factories.services.createTripService()
@@ -158,5 +188,5 @@ public class BeanTrips implements Serializable{
 		listaViajes.size());
 	return listaViajes;
     }
-    
+
 }
